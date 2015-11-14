e9b0ab2729d241cdcfee4f6c743f59a0eacc22a0
Pedro Gázquez <pedrogazqueznavarrete@gmail.com>
2015-11-14 20:20:05 +0100

    Update formu.py

--- formu.py
+++ formu.py
@@ -3,31 +3,7 @@ from flask import Flask, session, redirect, url_for, escape, request, render_tem
 import sys, pydoc
 from HTMLParser import HTMLParser
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
 app = Flask(__name__)
-#Expresion regular para que el numero de VISA sea corecto
-regVISA='(\d\d\d\d)[ \-](\d\d\d\d)[ \-](\d\d\d\d)[ \-](\d\d\d\d)$'
-listUsers = []
-listSurna = []
-
-class ComprobarEtiquetas(HTMLParser):
-	def handle_starttag(self, tag, attrs):
-		global ini_html
-		global hay_css
-		global hay_form
-		if tag=='html':
-			ini_html = 1
-		for attr in attrs:
-			if (attr[0] == 'href' and attr[1] == '/static/style.css'):
-				hay_css = 1
-		for attr in attrs:
-			if (attr[0] == 'method' and attr[1] == 'post'):
-				hay_form = 1
-	def handle_endtag(self, tag):
-		global fin_html
-		if tag=='html':
-			fin_html = 1
 
 @app.route('/')
 def index():
