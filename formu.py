c801ee7738deb4138cf2e15e353251dde89527d0
Pedro Gázquez <pedrogazqueznavarrete@gmail.com>
2015-11-15 13:05:33 +0100

    Update formu.py

--- formu.py
+++ formu.py
@@ -8,6 +8,6 @@ def index():
 	return render_template('index.html')
 	
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)  # 0.0.0.0 para permitir conexiones desde cualquier sitio. Ojo, peligroso en modo debug
-
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
 	
